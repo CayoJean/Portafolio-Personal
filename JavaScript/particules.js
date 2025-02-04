ce68130ace924b8bcdd9a83f0480dcd05dae3352
CayoJean <cayojean.fs_97@hotmail.com>
2025-02-04 10:53:07 -0500

    Cambios 2025

--- JavaScript/particules.js
+++ JavaScript/particules.js
@@ -1,65 +1,64 @@
-particlesJS('particles-js', {
-    particles: {
-        number: {
-            value: 100,
-            density: {
-                enable: true,
-                value_area: 800
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 100,
+            "density": {
+                "enable": true,
+                "value_area": 800
             }
         },
-        color: {
-            value: "#ff0000"  // Color rojo para las partículas
+        "color": {
+            "value": "#000000" // Cambiado a negro
         },
-        shape: {
-            type: "image",  // Usamos una imagen para la forma
-            image: {
-                src: "https://cdn-icons-png.flaticon.com/512/1532/1532322.png",  // URL de una imagen en forma de corazón
-                width: 20,
-                height: 20
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
             }
         },
-        opacity: {
-            value: 0.7,
-            random: true,
-            anim: {
-                enable: true,
-                speed: 1,
-                opacity_min: 0.1,
-                sync: false
-            }
+        "opacity": {
+            "value": 10,
+            "random": true
         },
-        size: {
-            value: 5,
-            random: true,
-            anim: {
-                enable: true,
-                speed: 4,
-                size_min: 0.1,
-                sync: false
-            }
+        "size": {
+            "value": 1.8,
+            "random": true
         },
-        move: {
-            enable: true,
-            speed: 3,
-            direction: "none",
-            random: true,
-            straight: false,
-            out_mode: "out",
-            bounce: false
+        "move": {
+            "enable": true,
+            "speed": 1,
+            "direction": "none",
+            "random": true,
+            "out_mode": "out",
+            "bounce": false
+        },
+        "line_linked": {
+            "enable": false
         }
     },
-    interactivity: {
-        detect_on: "canvas",
-        events: {
-            onhover: {
-                enable: true,
-                mode: "repulse"
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "repulse"
             },
-            onclick: {
-                enable: true,
-                mode: "push"
+            "onclick": {
+                "enable": true,
+                "mode": "push"
+            },
+            "resize": true
+        },
+        "modes": {
+            "repulse": {
+                "distance": 100,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
             }
         }
     },
-    retina_detect: true
+    "retina_detect": true
 });
